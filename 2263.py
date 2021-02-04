27112be84bb0eb58bb5334a4bdee601d7e2c7ab4
shb115 <78284538+shb115@users.noreply.github.com>
2021-02-04 11:44:16 +0900

    Update 2263.py

--- 2263.py
+++ 2263.py
@@ -1,7 +1,7 @@
 import sys
 sys.setrecursionlimit(10 ** 6) #재귀
 
-def preorder(in_start, in_end, post_start, post_end): #preorder를 출력하는 함수, 
+def findpreorder(in_start, in_end, post_start, post_end): #preorder를 출력하는 함수, 
     if in_start > in_end or post_start > post_end:    #in_start : inorder의 시작 index, in_end : inorder의 끝 index
         return                                        #post_start : postorder의 시작 index, post_end : postorder의 끝 index
                                                       #배열을 주고 받으면서 하면 메모리 초과가 나기 때문에 index를 이용
@@ -12,8 +12,8 @@ def preorder(in_start, in_end, post_start, post_end): #preorder를 출력하는 
     left = a - in_start                               #왼쪽 길이
     right = in_end - a                                #오른쪽 길이
 
-    preorder(in_start, in_start + left - 1, post_start, post_start + left - 1)  #새로운 앞 배열에 맞게 preorder 찾기
-    preorder(in_end - right + 1, in_end, post_end - right, post_end - 1)        #새로운 뒷 배열에 맞게 preorder 찾기
+    findpreorder(in_start, in_start + left - 1, post_start, post_start + left - 1)  #새로운 앞 배열에 맞게 preorder 찾기
+    findpreorder(in_end - right + 1, in_end, post_end - right, post_end - 1)        #새로운 뒷 배열에 맞게 preorder 찾기
 
 n = int(sys.stdin.readline()) #정점 개수 입력값
 
@@ -24,4 +24,4 @@ ind = [0] * (n + 1) #ind : 각 번호의 index를 저장해놓은 배열, 매번
 for i in range(n):
     ind[inorder[i]] = i
 
-preorder(0, n - 1, 0, n - 1) #전체부터 시작
+findpreorder(0, n - 1, 0, n - 1) #전체부터 시작
