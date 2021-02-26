f86cbd45ff256bb5c6e8db1885a63cfac88af77c
shb115 <78284538+shb115@users.noreply.github.com>
2021-02-26 10:12:46 +0900

    Add files via upload

---
+++ 9372.py
@@ -0,0 +1,11 @@
+import sys
+
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    N, M = map(int, sys.stdin.readline().split())
+
+    for i in range(M):
+        a, b = map(int, sys.stdin.readline().split())
+
+    print(N - 1)
