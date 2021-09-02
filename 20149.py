230576d6f18abf247188e23e8315df2214526110
shb115 <78284538+shb115@users.noreply.github.com>
2021-09-02 14:45:44 +0900

    Add files via upload

--- 20149.py
+++ 20149.py
@@ -1,43 +1,43 @@
+import sys
+
 def ccw(p1, p2, p3):
     x1, y1 = p1
     x2, y2 = p2
     x3, y3 = p3
-
+    
     return (x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3)
-
-def find_crosspoint(p1, p2, p3, p4):
-    x1, y1 = p1
-    x2, y2 = p2
-    x3, y3 = p3
-    x4, y4 = p4
-
-    if (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4) != 0:
-        Px = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4))
-        Py = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4))
-        print(Px, Py)
-
-x1, y1, x2, y2 = map(int, input().split())
-x3, y3, x4, y4 = map(int, input().split())
+    
+x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
+x3, y3, x4, y4 = map(int, sys.stdin.readline().split())
 
 p1, p2, p3, p4 = [x1, y1], [x2, y2], [x3, y3], [x4, y4]
 
-if ccw(p1, p2, p3) * ccw(p1, p2, p4) > 0 or ccw(p3, p4, p1) * ccw(p3, p4, p2) > 0:
-    print(0)
-elif ccw(p1, p2, p3) * ccw(p1, p2, p4) == 0 and ccw(p3, p4, p1) * ccw(p3, p4, p2) == 0:
-    if p1 >= p2:
-        p1, p2 = p2, p1
-
-    if p3 >= p4:
-        p3, p4 = p4, p3
-
-    if p4 >= p1 and p2 > p3:
-        print(1)
-        find_crosspoint(p1, p2, p3, p4)
-    elif p4 >= p1 and p2 == p3:
-        print(1)
-        print(*p2)
+if ccw(p1, p2, p3) * ccw(p1, p2, p4) <= 0 and ccw(p3, p4, p1) * ccw(p3, p4, p2) <= 0:
+    if ccw(p1, p2, p3) * ccw(p1, p2, p3) == 0 and ccw(p3, p4, p1) * ccw(p3, p4, p2) == 0:
+        if min(x1, x2) <= max(x3, x4) and min(x3, x4) <= max(x1, x2) and min(y1, y2) <= max(y3, y4) and min(y3, y4) <= max(y1, y2):
+            print(1)
+            if min(x1, x2) == max(x3, x4) and min(y1, y2) == max(y3, y4):
+                print(min(x1, x2), min(y1, y2))
+            elif min(x3, x4) == max(x1, x2) and min(y3, y4) == max(y1, y2):
+                print(min(x3, x4), min(y3, y4))
+            else:
+                if ccw(p1, p2, p4) * ccw(p3, p4, p1) != 0 or ccw(p1, p2, p3) * ccw(p3, p4, p1) != 0:
+                    print(*p2)
+                elif ccw(p1, p2, p4) * ccw(p3, p4, p2) != 0 or ccw(p1, p2, p3) * ccw(p3, p4, p2) != 0:
+                    print(*p1)
+        else:
+            print(0)
     else:
-        print(0)
+        print(1)
+        if x2 != x1 and x4 != x3:
+            g1, g2 = (y2 - y1) / (x2 - x1), (y4 - y3) / (x4 - x3)
+            b1, b2 = -g1 * x1 + y1, -g2 * x3 + y3
+            print((b1 - b2) / (g2 - g1), (g2 * b1 - g1 * b2) / (g2 - g1))
+        elif x2 == x1 and x4 != x3:
+            g2 = (y4 - y3) / (x4 - x3)
+            print(x1, g2 * (x1 - x3) + y3)
+        elif x4 == x3 and x2 != x1:
+            g1 = (y2 - y1) / (x2- x1)
+            print(x3, g1 * (x3 - x1) + y1)
 else:
-    print(1)
-    find_crosspoint(p1, p2, p3, p4)
+    print(0)
