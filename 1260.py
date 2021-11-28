9affda25db9b1557ffd345325cb7bbfb3542de5f
shb115 <shb115@naver.com>
2021-11-28 11:27:21 +0900

    no message

--- 1260.py
+++ 1260.py
@@ -1,15 +1,16 @@
 import sys
 from collections import deque
 
+# DFS
 def dfs(graph, start_node):
     visit = []
     stack = [start_node]
 
     for i in range(N):
-        graph[i + 1].sort(reverse=True)
+        graph[i + 1].sort(reverse = True)
 
     while stack:
-        node = stack.pop()
+        node = stack.pop()  # 가장 최근에 첨가된 노드를 선택
         if node not in visit:
             visit.append(node)
             stack.extend(graph[node])
@@ -17,8 +18,9 @@ def dfs(graph, start_node):
     for i in range(len(visit)):
         visit[i] = str(visit[i])
         
-    return ' '.join(visit)
+    print(' '.join(visit))
 
+# BFS
 def bfs(graph, start_node):
     visit = []
     queue = deque([start_node])
@@ -27,7 +29,7 @@ def bfs(graph, start_node):
         graph[i + 1].sort()
     
     while queue:
-        node = queue.popleft()
+        node = queue.popleft()  # 인접한 모든 정점을 우선 방문
         if node not in visit:
             visit.append(node)
             queue.extend(graph[node])
@@ -35,19 +37,22 @@ def bfs(graph, start_node):
     for i in range(len(visit)):
         visit[i] = str(visit[i])
 
-    return ' '.join(visit)
+    print(' '.join(visit))
 
-graph = dict()
 
+# 문제에서 주어진 N, M, V
 N, M, V = map(int,sys.stdin.readline().split())
 
+# 경로
+graph = dict()
 for i in range(N):
     graph[i + 1] = []
 
+# 문제에서 주어진 간선이 연결하는 두 정점의 번호
 for i in range(M):
     a, b = map(int,sys.stdin.readline().split())
     graph[a].append(b)
     graph[b].append(a)
 
-print(dfs(graph,V))
-print(bfs(graph,V))
+dfs(graph, V)
+bfs(graph, V)
